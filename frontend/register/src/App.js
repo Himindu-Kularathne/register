e41e5c7143a88b13311c180f9620e98278494827
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2025-01-27 13:28:18 +0530

    initial commit with frontend

---
+++ frontend/register/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'; 
+import './App.css'; // Import the CSS file
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  // get register name
+  const getRegister = async () => {
+    const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/register`);
+    const data = await response.json();
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    // Uncomment to fetch real data from backend
+    // getRegister();
+    console.log(process.env.REACT_APP_BACKEND_URL);
+    // sample data list of users
+    setUsers([
+      {
+        name: 'John Doe',
+        email: 'john@gmail.com',
+      },
+      {
+        name: 'Jane Doe here',
+        email: 'here@gmail.com',
+      }
+    ]);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Register</h1>
+      <div className="users-container">
+        {users.map((user, index) => (
+          <div className="user-card" key={index}>
+            <h3>{user.name}</h3>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
