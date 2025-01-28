d7aa551df05fb8e39bff17b3b305ad5baf8882b5
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2025-01-28 06:45:21 +0530

    change backend url with choero pub url

--- frontend/register/src/config.js
+++ frontend/register/src/config.js
@@ -1,2 +1,2 @@
-const REACT_APP_BACKEND_URL = 'http://localhost:3001';
+const REACT_APP_BACKEND_URL = 'https://ba66de11-8e34-4f6c-a05c-74170f0470a5-dev.e1-us-east-azure.choreoapis.dev/demo/register-backend/v1.0';
 export default REACT_APP_BACKEND_URL;
